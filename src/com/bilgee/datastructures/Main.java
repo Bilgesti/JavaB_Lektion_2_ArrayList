8ee136db95613d7c5838a33bb888d78da21b7052
Bilge927 <bilegdemberel.bayasgalanbayar@stud.sti.se>
2023-10-16 15:47:58 +0200

    1

--- src/com/bilgee/datastructures/Main.java
+++ src/com/bilgee/datastructures/Main.java
@@ -1,6 +1,10 @@
 package com.bilgee.datastructures;
 
+import org.w3c.dom.ls.LSException;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -12,7 +16,24 @@ public class Main {
         ArrayList<Integer> scoreList = new ArrayList<>(); // NOT- recommended
 
         // List + Arraylist
-        List<String> scoreList2 = new ArrayList(); // Recommended
+        List<Integer> scoreList2 = new ArrayList<>(20); // Recommended
+
+        scoreList2.add(100); // Add new index with, element: 100
+        scoreList2.add(200);
+        scoreList2.add(300);
+
+        scoreList2.set( 1,400 );
+
+        System.out.println(scoreList2);
+
+
+
+        // Initial ArrayList values
+        List<Integer> testList = new ArrayList<>(
+                Arrays.asList(0,150,500) // InitialSize = 3
+        );
+
+        System.out.println(testList);
 
 
     }
