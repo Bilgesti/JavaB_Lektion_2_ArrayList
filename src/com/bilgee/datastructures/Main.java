f11cb8bb3c633ee9f1c56ce24610d2393277f16a
Bilge927 <bilegdemberel.bayasgalanbayar@stud.sti.se>
2023-10-16 15:21:48 +0200

    1

---
+++ src/com/bilgee/datastructures/Main.java
@@ -0,0 +1,19 @@
+package com.bilgee.datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Fixed sized ??? begränsad storlek
+        int[] scores = { 0, 150, 500 }; // Delete not Add elements - Impossible!
+
+        // Arraylist
+        ArrayList<Integer> scoreList = new ArrayList<>(); // NOT- recommended
+
+        // List + Arraylist
+        List<String> scoreList2 = new ArrayList(); // Recommended
+
+
+    }
+}
